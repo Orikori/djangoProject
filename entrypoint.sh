1428ee7c18682322a90c9b74a23537424a9658aa
Dmitrii <dorvakd9@gmail.com>
2021-06-13 08:15:07 +0300

    collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 exec gunicorn djangoProject.wsgi:application -b 0.0.0.0:8000 --reload
